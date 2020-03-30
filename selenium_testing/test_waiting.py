75f662e0e996c88e6bbdfba7af8832607409ab8b
XFbeibei <1782343025@qq.com>
2020-03-30 23:55:15 +0800

    显式等待和隐式等待

--- selenium_testing/test_waiting.py
+++ selenium_testing/test_waiting.py
@@ -1,6 +1,7 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -11,16 +12,37 @@ class TestFirst:
         self.driver.get("https://testerhome.com/")
         self.driver.implicitly_wait(3)  # 使用隐示等待
 
+    def teardown(self):
+        sleep(3)
+        self.driver.quit()
+
+#第一种：自己编写隐式方法
     def test_wait(self):
         # sleep(3)    #直接等待
         self.driver.find_element(By.XPATH, '//*[@id="main-nav-menu"]/ul/li[4]/a').click()
 
-        def wait(x):
+        def wait(x):  # 必须传入一个参数，供until中的method传入一个方法
             return len(
                 self.driver.find_elements(By.XPATH, '//*[@id="hot_teams"]/div[1]/strong')) >= 1  # 为什么不可以使用element
 
-        WebDriverWait(self.driver, 10).until(wait)
+        WebDriverWait(self.driver, 10).until(wait)   #wait函数使用时不能使用()，否则会当作函数使用处理
         self.driver.find_element(By.XPATH, '//*[@title="霍格沃兹测试学院(hogwarts)"]').click()
+
+# 第一种：使用selenium自带方法
+    def test_wait1(self):
+        # sleep(3)    #直接等待
+        self.driver.find_element(By.XPATH, '//*[@id="main-nav-menu"]/ul/li[4]/a').click()
+
+#selenium还提供了一些内置方法，这样我们可以不用自己单独编写方法
+        # expected_conditions.element_to_be_clickable(By.XPATH, '//*[@id="hot_teams"]/div[1]/strong')
+        #element_to_be_clickable 等待直到元素可被点击
+        #presence_of_element_located 等待元素出现
+        #visibility_of_element_located  等待元素可见  等等。。
+
+        WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="hot_teams"]/div[1]/strong')))
+        self.driver.find_element(By.XPATH, '//*[@title="霍格沃兹测试学院(hogwarts)"]').click()
+
+
 
 
 '''
