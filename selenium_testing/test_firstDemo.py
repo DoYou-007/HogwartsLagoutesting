ca1b336075c72ce49ba741b71f6922b607a690d4
xieguojun <1782343025@qq.com>
2020-03-30 14:04:16 +0800

    selenium_first_demo1

--- selenium_testing/test_firstDemo.py
+++ selenium_testing/test_firstDemo.py
@@ -1,6 +1,13 @@
-from time import sleep
+'''
+打开页面；
+点击-社团标签；
+点击-霍格沃兹学院；
+访问页面第一条帖子
+'''
 
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class TestFirst:
@@ -15,6 +22,7 @@ class TestFirst:
 
     def test_case(self):
         self.driver.get("https://testerhome.com/")
-        self.driver.find_element_by_link_text('社团').click()
+        # self.driver.find_element_by_link_text('社团').click()
+        self.driver.find_element(By.LINK_TEXT, "社团")  # 第二种书写方式
         self.driver.find_element_by_link_text('霍格沃兹测试学院').click()
         self.driver.find_element_by_css_selector('.topic-22287 .node').click()
