f5be453105672c3c9841ed9027206b7e786755e1
xieguojun <1782343025@qq.com>
2020-04-09 13:47:45 +0800

    app自动化

---
+++ app_testing/test_appiumstartdemo.py
@@ -0,0 +1,10 @@
+from appium import webdriver
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10'
+desired_caps['deviceName'] = 'ec76ef18'
+desired_caps['appPackage'] = 'com.android.setting'
+desired_caps['appActivity'] = 'com.android.setting.Setting'
+driver = webdriver.Remote('http://localhost:4723wd/hub', desired_caps)  # http://localhost:4723wd/hub 这个用处是什么？
+driver.quit()
