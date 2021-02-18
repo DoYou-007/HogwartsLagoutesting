fa16c16fb879ed5a6206e8f8f5fed0ea7c318923
xieguojun <1782343025@qq.com>
2021-02-18 23:52:24 +0800

    新增的方法

--- request_test/httprunner_test/20210217/debugtalk.py
+++ request_test/httprunner_test/20210217/debugtalk.py
@@ -1,6 +1,8 @@
 import time
+import  random
 
 from httprunner import __version__
+from httprunner.response import ResponseObject
 
 
 def get_httprunner_version():
@@ -13,3 +15,11 @@ def sum_two(m, n):
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def get_folders_num(response:ResponseObject):
+    print('response===',response.resp_obj.json())
+    folders_num = response.resp_obj.json()['data']['folders']
+    return len(folders_num)
+
+def gen_random_title():
+    return  f"文件-{random.randint(25,80)}"
